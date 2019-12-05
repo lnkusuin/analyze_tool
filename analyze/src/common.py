4c0d41f73d9bc65ff97934479e35168b53e83f05
inkusu <lnkusuin@gmail.com>
2019-12-05 11:47:37 +0900

    logger_count処理を削除

--- analyze/src/common.py
+++ analyze/src/common.py
@@ -17,17 +17,3 @@ def get_logger(name):
     logger.addHandler(stream_handler)
 
     return logger
-
-
-def logger_count(start_time, boundary=5000):
-    incremental = boundary
-
-    def _(count, end_time, logger):
-        nonlocal start_time
-        nonlocal boundary
-
-        if count > boundary:
-            logger.info(count)
-            logger.info("経過時間: {}秒".format(end_time-start_time))
-            boundary += incremental
-    return _
