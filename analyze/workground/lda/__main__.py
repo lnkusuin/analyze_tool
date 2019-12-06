80cc6c3a2f20e3ee372b02075e76f5bf2c9f8b28
wakai <wakai@vogaro.co.jp>
2019-12-06 18:45:20 +0900

    LDA分析コマンドのMakefileの設定値を環境変数から参照するように変更

--- analyze/workground/lda/__main__.py
+++ analyze/workground/lda/__main__.py
@@ -6,10 +6,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../src'))
 
 import fire
 
-from lda.step1 import run as step1
-from lda.step2 import run as step2
-from lda.step3 import run as step3
-from lda.step4 import run as step4
+from step1 import run as step1
+from step2 import run as step2
+from step3 import run as step3
+from step4 import run as step4
 
 fire.Fire()
 
