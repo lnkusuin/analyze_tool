04f3bb4b8688d7b590d4079f6c00508a2678da29
wakai <wakai@vogaro.co.jp>
2019-12-06 15:20:02 +0900

    ldaコマンドをclass管理に変更

--- analyze/workground/lda/__main__.py
+++ analyze/workground/lda/__main__.py
@@ -5,36 +5,27 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../src'))
 
 import fire
-from lda.step1 import run as step1
-from lda.step2 import run as step2
-from lda.step3 import run as step3
-from lda.step4 import run as step4
 
 
-def main(path: str = "", step_id: int = 0, n_jobs=4, batch_size=1000, topic_id=5, font_path=""):
-    """コマンドラインツール"""
+class Command:
 
-    # FIXME 抽象化するかも サブコマンド管理でいいかもしれない
-    step_list = {1: step1, 2: step2, 3: step3, 4: step4}
+    def step1(self, path):
+        from lda.step1 import run
+        return run(path)
 
-    # テキストデータの前処理
-    if step_id == 1:
-        step1(path)
-    # 自然言語処理
-    elif step_id == 2:
-        step2(path, n_jobs=n_jobs, batch_size=batch_size)
-    # トピックモデルの学習とモデルの評価
-    elif step_id == 3:
-        step3(path, topic_id=topic_id)
-    # 可視化
-    elif step_id == 4:
-        step4(font_path=font_path)
-    else:
-        pass
-        # TODO すべて実行
+    def step2(self, path, n_jobs=4, batch_size=1000):
+        from lda.step2 import run
+        return run(path, n_jobs=n_jobs, batch_size=batch_size)
 
-    return ""
+    def step3(self, path, topic_id=5):
+        from lda.step3 import run
+        return run(path, topic_id=topic_id)
+
+    def step4(self, font_path):
+        from lda.step4 import run
+        return run(font_path=font_path)
 
 
-fire.Fire(main)
+
+fire.Fire(Command)
 
