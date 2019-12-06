6abdd9926bbb1960fc2bf26d5c83bd1b10fd079e
wakai <wakai@vogaro.co.jp>
2019-12-06 15:35:21 +0900

    ldaコマンドをfireに最適化

--- analyze/workground/lda/__main__.py
+++ analyze/workground/lda/__main__.py
@@ -6,26 +6,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../src'))
 
 import fire
 
+from lda.step1 import run as step1
+from lda.step2 import run as step2
+from lda.step3 import run as step3
+from lda.step4 import run as step4
 
-class Command:
-
-    def step1(self, path):
-        from lda.step1 import run
-        return run(path)
-
-    def step2(self, path, n_jobs=4, batch_size=1000):
-        from lda.step2 import run
-        return run(path, n_jobs=n_jobs, batch_size=batch_size)
-
-    def step3(self, path, topic_id=5):
-        from lda.step3 import run
-        return run(path, topic_id=topic_id)
-
-    def step4(self, font_path):
-        from lda.step4 import run
-        return run(font_path=font_path)
-
-
-
-fire.Fire(Command)
+fire.Fire()
 
