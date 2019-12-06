b33f5d431482cf44a962db64355936c4b2f29de5
wakai <wakai@vogaro.co.jp>
2019-12-06 18:39:52 +0900

    共通ロガーの設定を変更

--- analyze/src/common.py
+++ analyze/src/common.py
@@ -6,14 +6,10 @@ def get_logger(name):
     loggerの取得
     :return:
     """
+    logging.basicConfig(format="%(levelname)s:%(asctime)s:%(name)s:%(message)s")
     logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.INFO)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(logging.Formatter("%(levelname)s:%(asctime)s:%(name)s:%(message)s"))
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.addHandler(stream_handler)
 
     return logger
