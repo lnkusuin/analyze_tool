23e00f5a3ee6c6fa500a67c6aa72b71cc05afe53
wakai <wakai@vogaro.co.jp>
2019-12-11 12:01:33 +0900

    自然言語処理の解析結果にテキスト情報とハッシュタグ情報を追加 自然言語解析にハッシュタグを識別するように変更

--- analyze/src/context/lda/nlp/__init__.py
+++ analyze/src/context/lda/nlp/__init__.py
@@ -1,5 +1,6 @@
 import time
 import os
+import re
 import sys
 from pathlib import Path
 from functools import partial
@@ -11,12 +12,14 @@ from spacy.util import minibatch
 import pandas as pd
 
 from common import get_logger
-from context.nlp import nlp
+from context.nlp import nlp, HASH_TAG
 
 logger = get_logger(__name__)
 
 
 def process_nlp(batch_id, texts, output_dir, size):
+    from spacy.matcher import Matcher
+
     start_time = time.perf_counter()
     out_path = Path(output_dir) / ("%d.json" % batch_id)
     if out_path.exists():
@@ -24,9 +27,20 @@ def process_nlp(batch_id, texts, output_dir, size):
 
     logger.info("Processing batch {}".format(batch_id))
     with out_path.open("w", encoding="utf8") as f:
-        for doc in nlp().pipe(texts):
+        _nlp = nlp()
+        matcher = Matcher(_nlp.vocab)
+        matcher.add('HASHTAG', None, [{'ORTH': '#'}, {'IS_ASCII': True}])
+
+        for doc in _nlp.pipe(texts):
+
+            matches = matcher(doc)
+            spans = []
+            for match_id, start, end in matches:
+                spans.append(doc[start:end])
+            for span in spans:
+                span.merge()
+
             words = [{
-                "raw_text": doc.text,
                 "text": token.text,
                 "lemma": token.lemma_,
                 "pos": token.pos_,
@@ -36,7 +50,12 @@ def process_nlp(batch_id, texts, output_dir, size):
                 "is_alpha": token.is_alpha,
                 "is_stop": token.is_stop
             } for token in doc]
-            f.write(json.dumps(words, ensure_ascii=False))
+
+            f.write(json.dumps({
+                "text": doc.text,
+                "words": words,
+                "hash_tags": re.findall(HASH_TAG, doc.text)
+            }, ensure_ascii=False))
             f.write("\n")
 
     logger.info("Saved {} texts to {}.txt = ファイル総数:{} 単一の処理時間: {}".format(len(texts), batch_id, size, time.perf_counter() - start_time))
@@ -45,7 +64,6 @@ def process_nlp(batch_id, texts, output_dir, size):
 def run(path, output_dir, n_jobs: int = 4, batch_size: int = 1000):
     """ 自然言語解析"""
     logger.info("====自然言語解析を行います。====")
-    logger.info("辞書の作成を行います。")
     n_jobs = int(n_jobs)
     batch_size = int(batch_size)
 
@@ -56,11 +74,12 @@ def run(path, output_dir, n_jobs: int = 4, batch_size: int = 1000):
     df = pd.read_csv(open(path, 'rU'), encoding="utf-8", engine="c")
     df = df.dropna(how='all')
     docs = df['text'].tolist()
+    output_dir = output_dir(prefix="nlp")()
 
     partitions = minibatch(docs, size=batch_size)
     executor = Parallel(n_jobs=n_jobs, verbose=10, backend="multiprocessing", prefer="processes")
     do = delayed(partial(process_nlp, size=len(docs)/batch_size))
-    tasks = (do(i, batch, output_dir(prefix="nlp")()) for i, batch in enumerate(partitions))
+    tasks = (do(i, batch, output_dir) for i, batch in enumerate(partitions))
     executor(tasks)
 
     logger.info("====自然言語解析が完了しました。。====")
