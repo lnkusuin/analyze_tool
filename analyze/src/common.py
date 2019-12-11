c2a42923d6b16e688318fa74d5cf403800b4eb29
wakai <wakai@vogaro.co.jp>
2019-12-11 20:14:03 +0900

    ログ出力をファイルに変更

--- analyze/src/common.py
+++ analyze/src/common.py
@@ -1,12 +1,18 @@
 import logging
 
+log_path = "/tmp/analyzed_app.log"
+print("see log {}".format(log_path))
+
 
 def get_logger(name):
     """
     loggerの取得
     :return:
     """
-    logging.basicConfig(format="%(levelname)s:%(asctime)s:%(name)s:%(message)s")
+    logging.basicConfig(
+        format="%(levelname)s:%(asctime)s:%(name)s:%(message)s",
+        filename=log_path
+    )
     logging.getLogger("googleapiclient.discovery_cache").setLevel(logging.INFO)
 
     logger = logging.getLogger(name)
