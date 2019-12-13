88370fd06cabcc82abc1849d9cd602c987e82ac6
wakai <wakai@vogaro.co.jp>
2019-12-13 16:05:56 +0900

    ワードクラウドでの出力をやめ、縦の棒グラフに変更

--- analyze/workground/lda/4_classify.py
+++ analyze/workground/lda/4_classify.py
@@ -1,9 +1,8 @@
 import functools
 import os
+import itertools
 
-from wordcloud import WordCloud
 import matplotlib
-import matplotlib.pylab as plt
 matplotlib.font_manager._rebuild()
 matplotlib.rcParams['font.family'] = 'IPAexGothic'
 
@@ -17,6 +16,7 @@ from setting import get_save_local_path
 
 
 logger = get_logger(__file__)
+
 
 if __name__ == '__main__':
     results = []
@@ -44,71 +44,31 @@ if __name__ == '__main__':
 
     df = df.query("確率 >= 0.6")
     df = df.sort_values(by="トピックid")
-    df.to_csv(classify_dir_base("classify_result.csv"), index=False, encoding="utf_8_sig")
+    csv_path = classify_dir_base("classify_result.csv")
+    df.to_csv(csv_path, index=False, encoding="utf_8_sig")
+    logger.info("vd {}".format(csv_path))
 
     logger.info("既存テキストの振り分けを行いました。")
 
-
-    import math
-    logger.info("ワードクラウドを作成します(トピック数:{})".format(model.num_topics))
-    fig, axs = plt.subplots(ncols=1, nrows=model.num_topics, figsize=(10, 40))
-    axs = axs.flatten()
-
-    def color_func(word, font_size, position, orientation, random_state, font_path):
-        return 'darkturquoise'
-
-    from collections import Counter
+    hash_tags_list = []
+    words_list = []
 
     for i, t in enumerate(range(model.num_topics)):
+        topic_id = t+1
+
         filterd_item = [item for item in results if item[0] == (t+1)]
 
         _c = " ".join([item[3] for item in filterd_item]).split(" ")
-        _c = [_c for _c in _c if _c != ""]
-        label = ""
-        for l in Counter(_c).most_common(3):
-            label += " #{} ".format(l[0])
+        hash_tags = [_c for _c in _c if _c != ""]
+        for hash_tag in hash_tags:
+            hash_tags_list.append([topic_id, hash_tag])
 
-        words = " ".join([item[4] for item in filterd_item])
-        if len(words):
-            im = WordCloud(
-                font_path='/System/Library/Fonts/ヒラギノ明朝 ProN.ttc',
-                background_color='black',
-                color_func=color_func,
-                width=700, height=300,
-                random_state=0,
-                scale=2
-            ).generate(" ".join([item[4] for item in filterd_item]))
+        words = [item[4].split() for item in filterd_item]
+        flatt_words = list(itertools.chain.from_iterable(words))
+        for word in flatt_words:
+            words_list.append([topic_id, word])
 
-            print(label)
-            axs[i].imshow(im.recolor(colormap='Paired_r', random_state=244), alpha=0.98)
-            axs[i].axis('off')
-            axs[i].set_title(label, fontsize=10)
+    pd.DataFrame(hash_tags_list, columns=["topic_id", "hash_tag"]).to_csv(classify_dir_base("hash_tags.csv"), index=False)
+    pd.DataFrame(words_list, columns=["topic_id", "word"]).to_csv(classify_dir_base("words.csv"), index=False)
 
-    # vis
-    plt.tight_layout()
-    # save as png
-    plt.savefig(classify_dir_base('wordcloud{}.png'.format(model.num_topics)))
     logger.info("処理が完了しました。")
-
-    #
-    # df.to_csv("aaa_2.csv")
-    #
-    # # テキスト1
-    # df1 = df.query("~(トピック名=='不明1')")
-    # df1 = df1.query("~(トピック名=='不明2')")
-    # df1 = df1.query("確率 >= 0.6")
-    # df1_words = df1["ワード"].values.tolist()
-    # df1_words = list(map(lambda x: x.split(" "), df1_words))
-    # with open("df1_words", "wb") as f:
-    #     pickle.dump(df1_words, f)
-    #
-    # df1.to_csv("df1.csv")
-    #
-    # # テキスト2
-    # df2 = df.query("トピック名=='不明1' | トピック名=='不明2'")
-    # df2_words = df2["ワード"].values.tolist()
-    # df2_words = list(map(lambda x: x.split(" "), df2_words))
-    # with open("df2_words", "wb") as f:
-    #     pickle.dump(df2_words, f)
-    #
-    # df2.to_csv("df2.csv")
